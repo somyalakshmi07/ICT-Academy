39922fbaad755beda521dccc4d95294b298d13ea
somyalakshmi07 <somyalakshmi18@gmail.com>
2026-02-07 14:46:31 +0530

    js files learns

---
+++ script1.js
@@ -0,0 +1,9 @@
+let globalVar = "I am a global variable";
+let localVar = "I am a local variable";
+function fun(){
+    
+
+}
+fun();
+console.log(globalVar);
+console.log(localVar);
---
+++ scripts.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    console.log("hello");
+    console.log("how are you?");
+}
+myFunction();
+// my first code
+let geek="computer science portal";
+console.log(geek);
+
+let sum=5+8;
+console.log(sum);
+
+/* it is multi line comment
+it will not be displayed upon
+excetion og the code */
+console.log("This is multi line comment example");
